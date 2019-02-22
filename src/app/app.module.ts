406a295a2574574b2d5f94bc4c3ad7373828dcf4
Clayton McFarland <claytonmc150@gmail.com>
2019-02-22 11:53:22 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupFormComponent } from './signup-form/signup-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SignupFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
